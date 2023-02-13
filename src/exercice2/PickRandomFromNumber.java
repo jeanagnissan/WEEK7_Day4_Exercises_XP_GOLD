ff264db0157f0aa5175d049148233800080286cd
jeanagnissan <yoanefirst@gmail.com>
2023-02-13 14:03:34 +0000

    update app WEEK7_Day4_Exercises_XP_GOLD

---
+++ src/exercice2/PickRandomFromNumber.java
@@ -0,0 +1,9 @@
+package exercice2;
+
+import java.util.Random;
+
+public class PickRandomFromNumber {
+    public static int random(int a, int b) {
+        return new Random().nextInt(a, b + 1);
+    }
+}
---
+++ src/exercice3/PickRandomFromArray.java
@@ -0,0 +1,12 @@
+package exercice3;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PickRandomFromArray {
+    public static String random(ArrayList<String> values) {
+        // return a random string from the ones contained in values
+        int randomIndex = new Random().nextInt(0, values.size());
+        return values.get(randomIndex);
+    }
+}
